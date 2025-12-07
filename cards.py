adbf78817eaf0241f5faaa74a038f8697bce82a6
em-g73 <emerymgrace@gmail.com>
2025-12-07 01:05:48 -0500

    Updated Cards X-Values
    Updated the x-values of the cards to change depending on the card's pile. Added a pile attribute.

--- cards.py
+++ cards.py
@@ -4,7 +4,7 @@ from pygame.sprite import Sprite
 
 class Cards(Sprite):
 
-    def __init__(self, game, group, card):
+    def __init__(self, game, group, card, pile):
         super().__init__()
         #Sets up screen and settings
         self.screen = game.screen
@@ -14,17 +14,22 @@ class Cards(Sprite):
         #Sets the card and group variables to the card and group values defined where the function is called
         self.group = group
         self.card = card
+        self.pile = pile
 
-        self.create_attributes()
+        self.create_attributes(game)
 
         #Converts the numerical value of the coordinates to rectangular coordinates
         self.rect.x = self.x
         self.rect.y = self.y
 
-    def create_attributes(self): #Sets image and location
-        #Sets x and y to 0 (Change later)
-        self.x = 20
-        self.y = 20
+    def create_attributes(self, game): #Sets image and location
+        #Sets x and y (Change later)
+        if self.pile == 'draw_pile':
+            self.x = 20
+            self.y = 20
+        else:
+            self.x = game.columns_x_list[self.pile]
+            self.y = 20
 
         #Sets the image to the image corrosponding with the key and group
         self.image = pygame.image.load(f"images/{self.card}_{self.group}.png")
