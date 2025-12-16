50bfff42d8e83f045b4e412fe0a6d3a78c1e7e65
em-g73 <emerymgrace@gmail.com>
2025-12-15 22:36:50 -0500

    Added draw_pile_flip function
    Moved the function for flipping cards in the draw pile to Card.py, added a reverse side to cards in draw pile

--- main/cards.py
+++ main/cards.py
@@ -28,13 +28,14 @@ class Cards(Sprite):
         if self.pile == 'draw_pile':
             self.x = 20
             self.y = 20
+            self.image = pygame.image.load(f"images/reverse.png")
         else:
             self.x = game.columns_x_list[self.pile]
             self.y = game.columns_y_list[self.pile]
             game.columns_y_list[self.pile] += 75
+            #Sets the image to the image corrosponding with the key and group
+            self.image = pygame.image.load(f"images/{self.card}_{self.group}.png")
 
-        #Sets the image to the image corrosponding with the key and group
-        self.image = pygame.image.load(f"images/{self.card}_{self.group}.png")
         self.rect = self.image.get_rect()
     
     def create_highlight(self, game): #Creates a highlight around the card to show it has been clicked on
@@ -50,6 +51,14 @@ class Cards(Sprite):
             except AttributeError:
                 pass
         self.highlight = Highlight(game, self)
+
+    def draw_pile_flip(self, game):
+        self.y += 200
+        #change layer
+        game.draw_pile_list.pop()
+        game.draw_pile_list.insert(0, game.current_card)
+        self.image = pygame.image.load(f"images/{self.card}_{self.group}.png")
+        self.rect = self.image.get_rect()
 
     def blitme(self):
         #Copies the sprite image to the screen
